e942616b2b2d556aac5872032f8c46b1c9f9f183
Luis Rodrigues <luismiguelfr74@hotmail.com>
2026-01-02 20:46:14 +0000

    Added game settings from json.

--- src/Game.ts
+++ src/Game.ts
@@ -1,16 +1,39 @@
 import p5 from "p5";
 
+// Interface for game settings.
+interface ISettings {
+  name: string;
+  description: string;
+  pauseKey: string;
+  fps: number;
+  debug: boolean;
+}
+
 // Manages the game state.
 export default class Game {
   p5: p5;
+  settings!: ISettings;
 
   constructor(p5: p5) {
     this.p5 = p5;
 
-    p5.setup = () => {
+    p5.setup = async () => {
+      await this.p5.loadJSON("/game.json", (data: ISettings) => {
+        this.settings = data;
+        this.p5.frameRate(this.settings.fps);
+      });
+      if (this.settings.debug) {
+        console.debug("Game Settings:", this.settings);
+      }
+      console.info(`Game "${this.settings.name}" initialized.`);
     };
 
     p5.draw = () => {
+      const deltaTime = this.p5.deltaTime;
+      if (this.settings.debug) {
+        console.debug(`Delta Time: ${deltaTime} ms`);
+      }
+      this.p5.noLoop();
     };
   }
 }
